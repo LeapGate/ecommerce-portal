0a809295b29ecf526a71ea2ab7264ffece675cff
Leonardo Portal <leonardoportal12@gmail.com>
2022-05-12 00:00:19 -0300

    Update CartContext.js

--- src/componentes/CartContext.js
+++ src/componentes/CartContext.js
@@ -8,16 +8,33 @@ const { Provider} =  contexto
 
 const MiProvider = ({children}) =>{
     const [carrito, setCarrito] =  useState({})
+    const [cantidad_total, setCantidad_total] = useState(0)
+    const [precio_total, setPrecio_total] = useState(0)
 
+    const agregarProducto = (producto, cantidad) => {
 
+    }
+
+    const eliminarProducto = (id) =>{
+
+    }
+
+    const vaciarCart = () => {
+        setCarrito([])
+    }
+
+    const estaEnCart = (producto) =>{
+
+    }
 
 
     const valorContext = {
-        cantidad_total : 0,
-        precio_total :0,
-        carrito : []
+        cantidad_total,
+        precio_total,
+        carrito,
 
     }
+
     return(
         <Provider value = {{valorContext}}>
             {children}
