8a7fd2f0ff03f79e2896659e2e8fe091e25d4a9a
Leonardo Portal <leonardoportal12@gmail.com>
2022-05-23 22:52:18 -0300

    desafio clase 12

--- src/componentes/Cart.js
+++ src/componentes/Cart.js
@@ -2,19 +2,44 @@
 import { useContext, useState, useEffect } from "react"
 import { contexto } from "./CartContext"
 import { Link } from "react-router-dom"
-
+import {db} from "../firebase"
+import { collection, addDoc } from "firebase/firestore"
 
 
 const Cart = () => {
   const {carrito,precioTotal, eliminarProducto, vaciarCart} = useContext(contexto)
-  
-
+  const [compra, setCompra] = useState()
+    
     const eliminarItem = (id) =>{
       eliminarProducto(id)
+      
       
   }
 
   const handleTerminar = () =>{
+
+    const ordenesColeccion = collection(db,"ordenes")
+
+    const orden = {
+      buyer :{
+        nombre : "Leonardo",
+        telefono : "1667584",
+        email : "mailfalso@test.com"
+
+      },
+      items : carrito,
+      fecha : "",
+      total : precioTotal
+    }
+    const consulta = addDoc(ordenesColeccion,orden)
+
+    consulta
+    .then((res) =>{
+      setCompra(res.id)
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
     vaciarCart()
   }
   
@@ -36,6 +61,7 @@ const Cart = () => {
       <p className="item-precio" > Total de compra : ${precioTotal}</p>
 
       <button onClick = {handleTerminar}className="btn btn-secondary btn-compra"> Terminar</button>
+      {compra && <h3> Compra realizada con el id:{compra}</h3>}
 
     </>
   )
